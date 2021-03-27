b94188ead218cca30135b286a93395af9e0c2c6c
Eng4Ala2 <eng4ala2@gmail.com>
2021-03-27 22:00:44 +0300

    Blog(buton): onclick handler

--- src/component/home.js
+++ src/component/home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+  const onclickHandler = (name) => {
+    console.log("Hi from home! " + name);
+  };
   return ( 
     <div className="home">
       <h2>Here is your Home page</h2>
+      <button onClick={() => onclickHandler('Ala2')}>
+        Click!
+      </button>
     </div>
    );
 }
