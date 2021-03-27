9ff37a4f0844f6b9dfc34e75ea2d0ce6c2dfcab9
Eng4Ala2 <eng4ala2@gmail.com>
2021-03-27 22:44:51 +0300

    Blog(buton): useState

--- src/component/home.js
+++ src/component/home.js
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 const Home = () => {
-  const onclickHandler = (name) => {
-    console.log("Hi from home! " + name);
-  };
+  const [name, setName] = useState('Ala2')
+  const [age, setAge] = useState(24)
+
+  const onclickHandler = () => {
+    setName("Aseel");
+    setAge(20);
+  }
+
   return ( 
     <div className="home">
       <h2>Here is your Home page</h2>
-      <button onClick={() => onclickHandler('Ala2')}>
+      <p>{name} is {age} years old!</p>
+      <button onClick={() => onclickHandler()}>
         Click!
       </button>
     </div>
