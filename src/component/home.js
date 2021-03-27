78d8a5b189e689256a1303cf7890bf3f9fea465e
Eng4Ala2 <eng4ala2@gmail.com>
2021-03-27 21:19:03 +0300

    Blog(NavBar): Add nav bar and home component

---
+++ src/component/home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return ( 
+    <div className="home">
+      <h2>Here is your Home page</h2>
+    </div>
+   );
+}
+ 
+export default Home;
---
+++ src/component/navbar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return ( 
+    <nav className="navbar">
+      <h1>Hello to my Blogs!</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/about">about</a>
+        <a href="/creat">New Blog</a>
+      </div>
+    </nav>
+   );
+}
+ 
+export default NavBar;
