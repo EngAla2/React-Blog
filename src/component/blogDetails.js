fd73cd2761b6c1ee5c8f731cc252841efab50dbe
Eng4Ala2 <eng4ala2@gmail.com>
2021-05-30 02:12:20 +0300

    updates

--- src/component/blogDetails.js
+++ src/component/blogDetails.js
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import Loading from './loading';
 import useFetch from './useFetch';
 const BlogDetails = () => {
 
@@ -6,7 +7,15 @@ const BlogDetails = () => {
   const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id)
   return (
     <div className="blog-details">
-      { error ? <p>{error}</p> : isPending && <p>ppp</p>}
+
+      { error ? <p>{error}</p> : isPending && <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <Loading />
+      </div>
+      }
       { blog && (
         <article>
           <h2>{blog.title}</h2>
--- src/component/loading.js
+++ src/component/loading.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactLoading from 'react-loading';
 
-const Loading = ({ type, color }) => {
+const Loading = ({ type = "spinningBubbles", color = "#f1356d" }) => {
   return (
     <ReactLoading type={type} color={color} height={'20%'} width={'20%'} />
   );
